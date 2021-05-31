1f266736d985501bfefea3550fefdac9f1fa468a
Fe1c <tguyofawesome@gmail.com>
2021-05-31 15:34:55 -0700

    Wrote method that replaces multiples of 3 in array with fizz

--- fizz.py
+++ fizz.py
@@ -1,5 +1,9 @@
 def fizz():
     nums = []
     for x in range(100):
-        nums.append(x+1)
+        number = x+1
+        if(number % 3 == 0):
+            nums.append("fizz")
+        else:
+            nums.append(number)
     return nums
