f397b7bb6338b0b1385c25b516644bfe765ae2bf
Fe1c <tguyofawesome@gmail.com>
2021-05-31 15:41:26 -0700

    Wrote test to test if index five is 'Buzz'.

--- test_fizz.py
+++ test_fizz.py
@@ -10,8 +10,11 @@ class TestCase(unittest.TestCase):
         test = fizz.fizz() #test[0] should equal 1 as fizz returns the array sequence
         self.assertEqual(test[0], 1)
     def test_pass_2(self):
-        test = fizz.fizz() #test[0] should equal 1 as fizz returns the array sequence
+        test = fizz.fizz() #test[2] should be Fizz as 3 is divisible by 3
         self.assertEqual(test[2], "Fizz")
+    def test_pass_3(self):
+        test = fizz.fizz() #test[4] should be Buzz as 5 is divisible by 3
+        self.assertEqual(test[4], "Buzz")
 
 
 
